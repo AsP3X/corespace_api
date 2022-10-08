094288878980ed7451c028b411f10596d1d4bd1b
AsP3X <schockcrafter@gmail.com>
2022-10-08 22:33:31 +0200

    TASK: username must now be unique

--- src/models/users.js
+++ src/models/users.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   created: { type: Date, default: Date.now },
   lastLogin: { type: Date}
 });
