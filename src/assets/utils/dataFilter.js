18d4cd4402860bfc80714760767b80843fcb965e
AsP3X <schockcrafter@gmail.com>
2022-10-08 22:31:00 +0200

    TASK: added support for personal data filtering

--- src/assets/utils/dataFilter.js
+++ src/assets/utils/dataFilter.js
@@ -26,4 +26,19 @@ dataFilter.userData = (users, filter) => {
   return usersObj;
 }
 
+dataFilter.personalData = (user, filter) => {
+  if (!filter) { filter = "default" }
+
+  const dataFilterRaw = fs.readFileSync(path.resolve(__dirname, '../configs/dataFilter.json'));
+  const dataFilter = JSON.parse(dataFilterRaw);
+
+  const userObj = user.toJSON();
+
+  dataFilter.users[filter].forEach((field) => {
+    delete userObj[field];
+  });
+
+  return userObj;
+}
+
 module.exports = dataFilter;
